af54889f519af2ffe103eccf1bb9cdd6018736a6
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-03-20 03:04:06 -0700

    Add create-react-app frontend

--- src/main/java/com/github/srwaggon/adventure/AdventureApplication.java
+++ src/main/java/com/github/srwaggon/adventure/AdventureApplication.java
@@ -2,8 +2,6 @@ package com.github.srwaggon.adventure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AdventureApplication {
@@ -12,12 +10,4 @@ public class AdventureApplication {
     SpringApplication.run(AdventureApplication.class);
   }
 
-  @Controller
-  static class MainController {
-    @GetMapping("/")
-    public String getIndex() {
-      return "index";
-    }
-  }
-  
 }
