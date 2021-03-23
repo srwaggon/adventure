f79c6fb739e65bb172881ca5bb578ccb9bfaaf6e
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-03-23 02:33:55 -0700

    Decompose scripts

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,5 +2,5 @@ call scripts\clean.bat
 
 @echo off
 echo Building...
-call gradlew build
-docker build --build-arg JAR_FILE=build/libs/*.jar -t swaggoner/adventure .
+gradlew build
+
---
+++ scripts/docker_push.bat
@@ -0,0 +1,1 @@
+docker push swaggoner/adventure:latest
