11827df46dac89d5df203830f6c9e022cebfec7f
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-03-25 08:01:11 -0700

    Center character portraits

--- src/main/javascript/adventure/src/components/character/CharacterPortraitCard/CharacterPortraitCard.jsx
+++ src/main/javascript/adventure/src/components/character/CharacterPortraitCard/CharacterPortraitCard.jsx
@@ -6,6 +6,7 @@ import React from 'react';
 const CharacterPortraitCard = ({name}) =>
   <Card className={'character-portrait-card'}>
     <img className={'character-portrait-image'}
+         style={{marginLeft: '50%', transform: 'translateX(-50%)'}}
          alt={'character portrait'}
          src={'https://cdn.discordapp.com/attachments/823412384311279666/823443211988107264/Travin_the_Human.jpeg'}/>
     <Typography variant="h6">{name}</Typography>
