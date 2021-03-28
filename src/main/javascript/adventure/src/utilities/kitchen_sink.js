e74ad054f325efb5adf3891f802eae4f55c23a6a
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-03-28 07:30:16 -0700

    Add Array element removal functions

--- src/main/javascript/adventure/src/utilities/kitchen_sink.js
+++ src/main/javascript/adventure/src/utilities/kitchen_sink.js
@@ -1,2 +1,21 @@
 export const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 export const prettifyCardType = (cardType) => cardType.toLowerCase().split('_').map(capitalize).join(' ');
+export const arrayRemove = (array, value) => {
+  const index = array.indexOf(value);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+  return array;
+}
+
+export const arrayRemoveAll = (array, value) => {
+  let i = 0;
+  while (i < array.length) {
+    if (array[i] === value) {
+      array.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  return array;
+}
