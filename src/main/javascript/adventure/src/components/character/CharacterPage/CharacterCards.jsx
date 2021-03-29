d7cbba4c7eb2a53f7433dec1013b91c48fef2c0e
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-03-28 17:54:00 -0700

    Employ CardFilterAppBar in CharacterCards.jsx

--- src/main/javascript/adventure/src/components/character/CharacterPage/CharacterCards.jsx
+++ src/main/javascript/adventure/src/components/character/CharacterPage/CharacterCards.jsx
@@ -1,10 +1,10 @@
-import {AppBar, Box, Card, CardContent, MenuItem, TextField, Toolbar, Typography} from '@material-ui/core';
+import {Card, CardContent} from '@material-ui/core';
 import CardsGrid from '../../cards/CardsGrid';
 import DeleteButton from '../../buttons/DeleteButton';
 import {arrayRemoveAt} from '../../../utilities/kitchen_sink';
 import {replaceCharacter} from '../../../utilities/client';
 import React, {useState} from 'react';
-import CardTypeSelect from '../../cards/CardTypeSelect/CardTypeSelect';
+import CardFilterAppBar from '../../cards/CardFilterAppBar';
 
 const CharacterCards = ({
   isEditing,
@@ -14,24 +14,13 @@ const CharacterCards = ({
   fetchCharactersCards,
 }) => {
 
-  const [filter, setFilter] = useState({
-    name: '',
-    type: 'any',
-  });
-
-  const filterCard = ({name, type}) => filterName(name) && filterType(type);
-  const filterName = (name) => name.toLowerCase().includes(filter.name);
-  const filterType = (type) => type === filter.type || filter.type === 'any';
-
-  const filteredCards = filter.name.length > 0 || filter.type !== 'any'
-    ? cards.filter(filterCard)
-    : cards.slice(0, 8);
+  const [filterFunc, setFilterFunc] = useState(() => x => x);
 
   return <Card>
-    <AppBarFilter title={'Cards'} filter={filter} setFilter={setFilter}/>
+    <CardFilterAppBar title={'Cards'} setFilterFunc={setFilterFunc}/>
     <CardContent>
       <CardsGrid
-        cards={filteredCards}
+        cards={filterFunc(cards)}
         CardDecorator={({children, index}) =>
           <Card>
             {children}
@@ -53,36 +42,5 @@ const CharacterCards = ({
     </CardContent>
   </Card>;
 };
-
-const AppBarFilter = ({title, filter, setFilter}) =>
-  <AppBar color='default' position='static'>
-    <Toolbar>
-      <Box display={'flex'} flexDirection={'row'} width={'100%'} alignItems={'center'} flexWrap={'wrap'}>
-        <Box flexGrow={1} flexShrink={0}>
-          <Typography variant='h6'>{title}</Typography>
-        </Box>
-        <Box display={'flex'} flexGrow={2} flexShrink={0} justifyContent={'flex-end'} flexWrap={'wrap'}>
-          <Box flexGrow={1} flexShrink={0}>
-            <CardTypeSelect
-              defaultValue={'any'}
-              onSelectType={(type) => setFilter({...filter, type})}>
-              <MenuItem value={'any'}>Any</MenuItem>
-            </CardTypeSelect>
-          </Box>
-          <Box flexGrow={1} flexShrink={0} pl={1}>
-            <TextField
-              label='Filter by name'
-              variant='outlined'
-              margin='dense'
-              fullWidth
-              defaultValue={filter.term}
-              onChange={event => setFilter({...filter, name: event.target.value.toLowerCase()})}
-            />
-          </Box>
-        </Box>
-      </Box>
-    </Toolbar>
-  </AppBar>
-;
 
 export default CharacterCards;
