8fdfb2913669dc61c24102cdaa9de0c037d74110
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-03-30 13:00:16 -0700

    Add QualityFilter to CardFilter.jsx
    And fix minor issues

--- src/main/javascript/adventure/src/components/cards/CardFilter.jsx
+++ src/main/javascript/adventure/src/components/cards/CardFilter.jsx
@@ -1,30 +1,44 @@
 import {Box, MenuItem, TextField} from '@material-ui/core';
 import CardTypeSelect from './CardTypeSelect/CardTypeSelect';
 import React, {useEffect, useState} from 'react';
+import CardQualitySelect from './CardQualitySelect';
 
 const filterName = (name) => (card) => card.name.toLowerCase().includes(name);
 const filterType = (type) => (card) => 'any' === type || type === card.type;
+const filterQuality = (quality) => (card) => 'any' === quality || quality === card.quality;
 
 const CardFilter = ({setFilterFunc}) => {
 
-  const [filter, setFilter] = useState({name: '', type: 'any'});
+  const [filter, setFilter] = useState({
+    name: '',
+    type: 'any',
+    quality: 'any',
+  });
 
   useEffect(() => {
-    const wholeFilter = (cards) =>
+    setFilterFunc(() => (cards) =>
       cards
         .filter(filterName(filter.name))
-        .filter(filterType(filter.type));
-    setFilterFunc(() => wholeFilter);
-  }, [filter.name, filter.type, setFilterFunc]);
+        .filter(filterType(filter.type))
+        .filter(filterQuality(filter.quality)),
+    );
+  }, [filter.name, filter.type, filter.quality, setFilterFunc]);
 
   return <Box display={'flex'} flexGrow={2} flexShrink={0}
               justifyContent={'flex-end'} flexWrap={'wrap'}>
-    <Box flexGrow={1} flexShrink={0}>
+    <Box flexGrow={1} flexShrink={0} pl={1}>
       <CardTypeSelect
         defaultValue={'any'}
-        onSelectType={(type) => setFilter({...filter, type})}>
+        onSelect={(type) => setFilter({...filter, type})}>
         <MenuItem value={'any'}>Any</MenuItem>
       </CardTypeSelect>
+    </Box>
+    <Box flexGrow={1} flexShrink={0} pl={1}>
+      <CardQualitySelect
+        defaultValue={'any'}
+        onSelect={(quality) => setFilter({...filter, quality})}>
+        <MenuItem value={'any'}>Any</MenuItem>
+      </CardQualitySelect>
     </Box>
     <Box flexGrow={1} flexShrink={0} pl={1}>
       <TextField
