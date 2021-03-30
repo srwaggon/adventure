a65af5af171a311c68570646f231899061553fe5
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-03-30 13:02:08 -0700

    Change field order in CardFilter.jsx

--- src/main/javascript/adventure/src/components/cards/CardFilter.jsx
+++ src/main/javascript/adventure/src/components/cards/CardFilter.jsx
@@ -24,22 +24,8 @@ const CardFilter = ({setFilterFunc}) => {
     );
   }, [filter.name, filter.type, filter.quality, setFilterFunc]);
 
-  return <Box display={'flex'} flexGrow={2} flexShrink={0}
-              justifyContent={'flex-end'} flexWrap={'wrap'}>
-    <Box flexGrow={1} flexShrink={0} pl={1}>
-      <CardTypeSelect
-        defaultValue={'any'}
-        onSelect={(type) => setFilter({...filter, type})}>
-        <MenuItem value={'any'}>Any</MenuItem>
-      </CardTypeSelect>
-    </Box>
-    <Box flexGrow={1} flexShrink={0} pl={1}>
-      <CardQualitySelect
-        defaultValue={'any'}
-        onSelect={(quality) => setFilter({...filter, quality})}>
-        <MenuItem value={'any'}>Any</MenuItem>
-      </CardQualitySelect>
-    </Box>
+  return <Box display={'flex'} flexGrow={2} flexShrink={0} justifyContent={'flex-end'} flexWrap={'wrap'}>
+
     <Box flexGrow={1} flexShrink={0} pl={1}>
       <TextField
         label='Filter by name'
@@ -50,6 +36,23 @@ const CardFilter = ({setFilterFunc}) => {
         onChange={event => setFilter({...filter, name: event.target.value.toLowerCase()})}
       />
     </Box>
+
+    <Box flexGrow={1} flexShrink={0} pl={1}>
+      <CardTypeSelect
+        defaultValue={'any'}
+        onSelect={(type) => setFilter({...filter, type})}>
+        <MenuItem value={'any'}>Any</MenuItem>
+      </CardTypeSelect>
+    </Box>
+
+    <Box flexGrow={1} flexShrink={0} pl={1}>
+      <CardQualitySelect
+        defaultValue={'any'}
+        onSelect={(quality) => setFilter({...filter, quality})}>
+        <MenuItem value={'any'}>Any</MenuItem>
+      </CardQualitySelect>
+    </Box>
+
   </Box>;
 };
 
