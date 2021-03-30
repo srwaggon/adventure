9554b613521f99b16aafba5bf4d30827b7878990
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-03-30 12:43:06 -0700

    Fix issues in CardQualitySelect.jsx

--- src/main/javascript/adventure/src/components/cards/CardQualitySelect.jsx
+++ src/main/javascript/adventure/src/components/cards/CardQualitySelect.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {getCardTypes} from '../../utilities/client';
+import {getCardQualities} from '../../utilities/client';
 import CardSelect from './../shared/CardSelect';
 
 const CardQualitySelect = ({children, defaultValue, onSelect}) => {
   const populator = (setValues) =>
-    getCardTypes()
+    getCardQualities()
       .then(response => response.json())
       .then(json => setValues(json));
 
-  return <CardSelect {...{populator, label: 'Type', defaultValue, onSelect, children}}/>;
+  return <CardSelect {...{populator, label: 'Quality', defaultValue, onSelect, children}}/>;
 };
 
 export default CardQualitySelect;
