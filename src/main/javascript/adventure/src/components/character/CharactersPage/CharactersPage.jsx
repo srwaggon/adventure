cbebfd1ac071813554d26188ce2cfd4737d51380
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-03-30 17:41:43 -0700

    Make CharactersPage use CenteredGridWithAppBar

--- src/main/javascript/adventure/src/components/character/CharactersPage/CharactersPage.jsx
+++ src/main/javascript/adventure/src/components/character/CharactersPage/CharactersPage.jsx
@@ -4,11 +4,12 @@ import React from 'react';
 
 import {postNewCharacter} from '../../../utilities/client';
 import {Link, useHistory} from 'react-router-dom';
-import {AppBar, Box, Card, Container, Grid, Toolbar, Typography} from '@material-ui/core';
+import {Box, Card} from '@material-ui/core';
 import CharacterPortraitCard from '../CharacterPortraitCard/CharacterPortraitCard';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import AddButton from '../../buttons/AddButton';
 import useCurrentPlayersCharacters from './UseCurrentPlayersCharacters';
+import CenteredGridWithAppBar from '../../shared/CenteredGridWithAppBar';
 
 const CharactersPage = () => {
   return (<div className={'HomePage'}>
@@ -32,29 +33,17 @@ const CharacterSelectionPage = () => {
       .then(response => response.json())
       .then(character => history.push(`/characters/${character.id}`));
 
-  return <div className={'character-selection-page'}>
-    <AppBar color="default" position={'static'}>
-      <Toolbar>
-        <Typography variant={'h6'} style={{flexGrow: 1}}>Select a Character</Typography>
-        <AddButton onClick={onClickCreateCharacter}/>
-      </Toolbar>
-    </AppBar>
-    <Container>
-      <Box p={4}>
-        <Grid className={'character-selections-panel'} container justify="center" spacing={4}>
-          {characters && characters.length > 0
-            ? characterPortraits.map(x => <Grid item>{x}</Grid>)
-            : <Grid item>
-              <Card className={'character-new-card character-panel'} onClick={onClickCreateCharacter}>
-                <Box flexDirection={'column'} justifyContent={'center'} height={'100%'}>
-                  <PersonAddIcon style={{fontSize: 128, margin: '0 auto', height: '100%'}}/>
-                </Box>
-              </Card>
-            </Grid>}
-        </Grid>
-      </Box>
-    </Container>
-  </div>;
+  return <CenteredGridWithAppBar
+    title={'Select a Character'}
+    menuItems={<AddButton onClick={onClickCreateCharacter}/>}>
+    {characters && characters.length > 0
+      ? characterPortraits
+      : <Card className={'character-new-card character-panel'} onClick={onClickCreateCharacter}>
+        <Box flexDirection={'column'} justifyContent={'center'} height={'100%'}>
+          <PersonAddIcon style={{fontSize: 128, margin: '0 auto', height: '100%'}}/>
+        </Box>
+      </Card>}
+  </CenteredGridWithAppBar>;
 };
 
 export default CharactersPage;
