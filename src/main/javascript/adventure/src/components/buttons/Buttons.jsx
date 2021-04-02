6d329b0935fcbdad2e4c5723feab72fca01d8723
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-04-02 15:25:51 -0700

    IconButtons pass down className property

--- src/main/javascript/adventure/src/components/buttons/Buttons.jsx
+++ src/main/javascript/adventure/src/components/buttons/Buttons.jsx
@@ -5,38 +5,44 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import FaceIcon from '@material-ui/icons/Face';
 import MenuIcon from '@material-ui/icons/Menu';
 
-export const AccountCircleButton = ({disabled, onClick}) =>
-  <StandardIconButton disabled={disabled} onClick={onClick}>
+export const AccountCircleButton = ({disabled, onClick, className}) =>
+  <StandardIconButton disabled={disabled} onClick={onClick} className={className}>
     <AccountCircleIcon/>
   </StandardIconButton>;
 
-export const ChevronLeftButton = ({disabled, onClick}) =>
-  <StandardIconButton disabled={disabled} onClick={onClick}>
+export const ChevronLeftButton = ({disabled, onClick, className}) =>
+  <StandardIconButton disabled={disabled} onClick={onClick} className={className}>
     <ChevronLeftIcon/>
   </StandardIconButton>;
 
-export const ChevronRightButton = ({disabled, onClick}) =>
-  <StandardIconButton disabled={disabled} onClick={onClick}>
+export const ChevronRightButton = ({disabled, onClick, className}) =>
+  <StandardIconButton disabled={disabled} onClick={onClick} className={className}>
     <ChevronRightIcon/>
   </StandardIconButton>;
 
-export const FaceButton = ({disabled, onClick}) =>
-  <StandardIconButton disabled={disabled} onClick={onClick}>
+export const FaceButton = ({disabled, onClick, className}) =>
+  <StandardIconButton disabled={disabled} onClick={onClick} className={className}>
     <FaceIcon/>
   </StandardIconButton>;
 
-export const MenuButton = ({disabled, onClick}) =>
-  <StandardIconButton disabled={disabled} onClick={onClick}>
+export const MenuButton = ({disabled, onClick, className}) =>
+  <StandardIconButton
+    disabled={disabled}
+    onClick={onClick}
+    className={className}
+  >
     <MenuIcon/>
   </StandardIconButton>;
 
-const StandardIconButton = ({children, disabled, onClick}) => {
+const StandardIconButton = ({children, disabled, onClick, className}) => {
   return <Box>
     <IconButton
       aria-label={'cancel'}
       color={'default'}
       disabled={disabled}
-      onClick={onClick}>
+      onClick={onClick}
+      className={className}
+    >
       {children}
     </IconButton>
   </Box>;
