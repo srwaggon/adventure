e50d6ba56d78b6aef7fae398d282c815a0e2751f
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-04-02 18:53:57 -0700

    Center Character Portrait and Character Details on Character Page

--- src/main/javascript/adventure/src/components/character/CharacterDetails.jsx
+++ src/main/javascript/adventure/src/components/character/CharacterDetails.jsx
@@ -15,7 +15,7 @@ const CharacterDetails = ({character, setCharacter, isEditing}) => {
 
   return <Box>
     <Box display='flex' flexDirection='row-reverse' flexWrap='wrap' justifyContent='space-between'>
-      <Box>
+      <Box display='flex' flexGrow={1} justifyContent='center'>
         <CharacterPortraitCard {...character}/>
         {isEditing && <Box py={1}>
           <TextField
@@ -27,50 +27,53 @@ const CharacterDetails = ({character, setCharacter, isEditing}) => {
             onChange={event => setCharacter({...character, portraitUrl: event.target.value})}/>
         </Box>}
       </Box>
-      <Box>
-        <Box display='flex' flexGrow={3} p={1}>
-          {isEditing
-            ? <EditCharacterNameTextField character={character} setCharacter={setCharacter}/>
-            : <Typography align='center' variant={'h3'}>{character.name}</Typography>}
-        </Box>
-        <Divider/>
-        <Typography variant='h5'>Attributes</Typography>
-        <Box display='flex' flexWrap='wrap'>
-          <Box pr={2}>
-            <CharacterAttribute {...characterPageState} attribute={'strength'}/>
-            <CharacterAttribute {...characterPageState} attribute={'dexterity'}/>
-            <CharacterAttribute {...characterPageState} attribute={'constitution'}/>
+
+      <Box display='flex' flexGrow={1} justifyContent='center'>
+        <Box>
+          <Box display='flex' flexGrow={3} p={1}>
+            {isEditing
+              ? <EditCharacterNameTextField character={character} setCharacter={setCharacter}/>
+              : <Typography align='center' variant={'h3'}>{character.name}</Typography>}
           </Box>
-          <Box pr={2}>
-            <CharacterAttribute {...characterPageState} attribute={'presence'}/>
-            <CharacterAttribute {...characterPageState} attribute={'influence'}/>
-            <CharacterAttribute {...characterPageState} attribute={'composure'}/>
+          <Divider/>
+          <Typography variant='h5'>Attributes</Typography>
+          <Box display='flex' flexWrap='wrap'>
+            <Box pr={2}>
+              <CharacterAttribute {...characterPageState} attribute={'strength'}/>
+              <CharacterAttribute {...characterPageState} attribute={'dexterity'}/>
+              <CharacterAttribute {...characterPageState} attribute={'constitution'}/>
+            </Box>
+            <Box pr={2}>
+              <CharacterAttribute {...characterPageState} attribute={'presence'}/>
+              <CharacterAttribute {...characterPageState} attribute={'influence'}/>
+              <CharacterAttribute {...characterPageState} attribute={'composure'}/>
+            </Box>
+            <Box pr={2}>
+              <CharacterAttribute {...characterPageState} attribute={'intelligence'}/>
+              <CharacterAttribute {...characterPageState} attribute={'wits'}/>
+              <CharacterAttribute {...characterPageState} attribute={'resolve'}/>
+            </Box>
           </Box>
-          <Box pr={2}>
-            <CharacterAttribute {...characterPageState} attribute={'intelligence'}/>
-            <CharacterAttribute {...characterPageState} attribute={'wits'}/>
-            <CharacterAttribute {...characterPageState} attribute={'resolve'}/>
+          <Divider/>
+          <Typography variant='h5'>Resources</Typography>
+          <Box display='flex' flexWrap='wrap'>
+            <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'stamina'}/></Box>
+            <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'confidence'}/></Box>
+            <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'focus'}/></Box>
           </Box>
-        </Box>
-        <Divider/>
-        <Typography variant='h5'>Resources</Typography>
-        <Box display='flex' flexWrap='wrap'>
-          <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'stamina'}/></Box>
-          <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'confidence'}/></Box>
-          <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'focus'}/></Box>
-        </Box>
-        <Box display='flex' flexWrap='wrap'>
-          <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'health'}/></Box>
-          <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'reputation'}/></Box>
-          <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'mana'}/></Box>
+          <Box display='flex' flexWrap='wrap'>
+            <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'health'}/></Box>
+            <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'reputation'}/></Box>
+            <Box pr={'16px'}><CharacterResource {...characterPageState} resource={'mana'}/></Box>
+          </Box>
+          <Divider/>
+          <Typography variant='h5'>Proficiencies</Typography>
+          <Box display='flex' flexWrap='wrap' flexDirection={'row'}>
+            {proficiencies.map(proficiency =>
+              <ProficiencyChip {...characterPageState} proficiency={proficiency}/>)}
+          </Box>
         </Box>
       </Box>
-    </Box>
-    <Divider/>
-    <Typography variant='h5'>Proficiencies</Typography>
-    <Box display='flex' flexWrap='wrap' flexDirection={'row'}>
-      {proficiencies.map(proficiency =>
-        <ProficiencyChip {...characterPageState} proficiency={proficiency}/>)}
     </Box>
   </Box>;
 };
