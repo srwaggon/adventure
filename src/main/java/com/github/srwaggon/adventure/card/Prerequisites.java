bfbade4f702b6b5eca4f3b89fd5ea3d1129f1073
Samuel Waggoner <samuel.waggoner@gmail.com>
2022-06-14 19:08:02 -0700

    Move away from javafx pair (in preparation for upgrade)

--- src/main/java/com/github/srwaggon/adventure/card/Prerequisites.java
+++ src/main/java/com/github/srwaggon/adventure/card/Prerequisites.java
@@ -1,29 +1,29 @@
 package com.github.srwaggon.adventure.card;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
-import javafx.util.Pair;
-
 public class Prerequisites {
-  private List<Pair<String, Integer>> attributePrerequisites = new ArrayList<>();
-  private List<Pair<String, Integer>> skillPrerequisites = new ArrayList<>();
+  private List<Map.Entry<String, Integer>> attributePrerequisites = new ArrayList<>();
+  private List<Map.Entry<String, Integer>> skillPrerequisites = new ArrayList<>();
   private List<UUID> cardPrerequisites = new ArrayList<>();
 
-  public List<Pair<String, Integer>> getAttributePrerequisites() {
+  public List<Map.Entry<String, Integer>> getAttributePrerequisites() {
     return attributePrerequisites;
   }
 
-  public void setAttributePrerequisites(List<Pair<String, Integer>> attributePrerequisites) {
+  public void setAttributePrerequisites(List<Map.Entry<String, Integer>> attributePrerequisites) {
     this.attributePrerequisites = attributePrerequisites;
   }
 
-  public List<Pair<String, Integer>> getSkillPrerequisites() {
+  public List<Map.Entry<String, Integer>> getSkillPrerequisites() {
     return skillPrerequisites;
   }
 
-  public void setSkillPrerequisites(List<Pair<String, Integer>> skillPrerequisites) {
+  public void setSkillPrerequisites(List<Map.Entry<String, Integer>> skillPrerequisites) {
     this.skillPrerequisites = skillPrerequisites;
   }
 
