9e6e34144be57c063f41077b5aedde9c593a3ce5
Samuel Waggoner <samuel.waggoner@gmail.com>
2022-07-02 22:42:33 -0700

    Quality dot retains shape (always a circle)

--- src/main/javascript/adventure/src/components/cards/VisualCard/CardQualityDot.jsx
+++ src/main/javascript/adventure/src/components/cards/VisualCard/CardQualityDot.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import {prettify} from "../../../utilities/kitchen_sink";
 import {getQualityColor} from "../../../utilities/colors";
+import {Box} from "@mui/material";
 
 export const CardQualityDot = ({quality}) => {
 
   quality = quality || "COMMON";
 
-  return <span
-    className="visual-card-quality-dot"
-    style={{backgroundColor: getQualityColor(quality)}}
-    title={prettify(quality)}
-  />;
+  return <Box width={10} height={10}>
+    <span
+      className="visual-card-quality-dot"
+      style={{backgroundColor: getQualityColor(quality)}}
+      title={prettify(quality)}
+    />
+  </Box>;
 };
