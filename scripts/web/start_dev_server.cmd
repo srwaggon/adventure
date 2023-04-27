5369424228d996c26cd6cc566cf333cf1c440b3d
Samuel Waggoner <samuel.waggoner@gmail.com>
2023-04-27 01:58:04 -0700

    add top-level start_dev_server.cmd

---
+++ scripts/web/start_dev_server.cmd
@@ -0,0 +1,4 @@
+cd src\main\javascript\adventure\
+yarn start
+
+# wsl scripts/web/start_dev_server.sh
---
+++ scripts/web/start_dev_server.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+WEB_SERVER_ROOT_DIR=./src/main/javascript/adventure
+SCRIPTS_DIR="$WEB_SERVER_ROOT_DIR/scripts"
+
+cd $WEB_SERVER_ROOT_DIR
+. scripts/start_dev_server.sh
