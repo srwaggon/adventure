b8f8cdc5d7db596e1e24a8b83d91ba3fa49d0635
Samuel Waggoner <samuel.waggoner@gmail.com>
2023-10-09 13:50:46 -0700

    Fix issue where new cards were saved with the ID "undefined"

--- src/main/javascript/adventure/src/components/cards/card/UseNewCard.jsx
+++ src/main/javascript/adventure/src/components/cards/card/UseNewCard.jsx
@@ -1,4 +1,5 @@
 export const useNewCard = () => ({
+  id: null,
   name: "New Card",
   fullArt: true,
   image: "https://cdn.discordapp.com/attachments/954643538342182924/963221684985409546/unknown.png",
